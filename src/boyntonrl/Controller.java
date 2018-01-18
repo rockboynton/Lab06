ae7d76fdb989bc42376324ff659fe67446b960e7
rockboynton <rock.boynton@yahoo.com>
2018-01-17 23:04:05 -0600

    Initial commit

---
+++ src/boyntonrl/Controller.java
@@ -0,0 +1,41 @@
+package boyntonrl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+
+public class Controller {
+
+    @FXML
+    private TextField urlTextField;
+    @FXML
+    private Button analyzeButton;
+    @FXML
+    private TextField sizeTextField;
+    @FXML
+    private TextField downloadTimeTextField;
+    @FXML
+    private TextField portTextField;
+    @FXML
+    private TextField hostTextField;
+    @FXML
+    private TextField timeoutTextField;
+    @FXML
+    private Button setButton;
+    @FXML
+    private TextArea contentTextArea;
+
+    @FXML
+    private void analyze(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void setTimeout(ActionEvent event) {
+
+    }
+}
